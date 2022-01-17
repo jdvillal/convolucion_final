a3f3c5679546393797e6a850eff44acbfce7e950
Jorge <daniel.--96@hotmail.com>
2022-01-17 14:54:00 -0500

    Arreglos menores

--- data_structs.c
+++ data_structs.c
@@ -1,1 +1,35 @@
 #include "data_structs.h"
+
+//================================================================//
+
+void str_enqueue(struct str_queue *cola, struct str_queue_node *nuevo_nodo){
+  if(cola->size == 0){
+    cola->head = nuevo_nodo;
+    cola->tail = nuevo_nodo;
+  }else{
+    cola->tail->next = nuevo_nodo;
+    cola->tail = nuevo_nodo;
+  }
+  cola->tamano++;
+}
+
+//Desencolar un elemento en el queue de forma segura
+int str_dequeue(struct str_queue *cola, struct str_queue_node *nodo_extraido){
+  nodo_extraido = cola->head;
+  if(cola->size > 0){
+    cola->head = cola->head->next;
+    cola->size--;
+    return cola->size;
+  }else{
+    return -1;
+  }
+}
+
+
+//================================================================//
+
+
+void cll_next(struct cll_imgs *list, struct cll_img_node *node){
+  node = cll_img_node->current_head;
+   
+}
--- data_structs.h
+++ data_structs.h
@@ -1,2 +1,34 @@
 #include <stdio.h>
 
+//=============================================================//
+struct str_queue_node{
+  struct str_queue_node *next;
+  char *str_data;
+};
+
+struct str_queue{
+  int size;
+  struct str_queue_node *head;
+  struct str_queue_node *tail;
+};
+
+
+
+
+
+//=============================================================//
+struct cll_img_node{
+  struct string_node *next;
+  char *datos;
+};
+
+typedef struct {
+    int size;
+    struct cll_img_node *current_head;
+}cll_imgs; //lista circular enlazada de imagenes
+
+
+
+
+
+
